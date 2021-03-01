a0363569b643ac34cfe4f4a848c7d6dbe3552762
NicholasZhurauliou <nicholaszhurauliou@gmail.com>
2021-03-01 14:12:34 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+      BYN:
+      <input type="text" 
+             :value="valueBYN" 
+            @input="calculate($event.target.value, 'BYN')" 
+      />
+    </label>
+    <label>
+      USD:
+      <input type="text" 
+             :value="valueUSD" 
+            @input="calculate($event.target.value, 'USD')" 
+      />
+    </label>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      valueBYN: '',
+      valueUSD: ''
+    }
+  },
+  methods: {
+    calculate(value, currency) {
+      const regexp = /^\d+([.,]\d*)?$/;
+      const rate  = 3;
+      const percent = 5;
+      const multiplier = 1 - percent / 100;
+    
+      if (value === '') {
+        this.valueUSD = '';
+        this.valueBYN = '';
+      } else if (!regexp.test(value)) {
+        this.$forceUpdate();
+      } else {
+        const newValue = +value.replace(',', '.'); 
+    
+        switch(currency) {
+          case 'USD': {
+            this.valueUSD = newValue;
+            this.valueBYN = newValue * multiplier * rate;
+            break;
+          }
+          case 'BYN': {
+            this.valueBYN = newValue;
+            this.valueUSD = newValue * multiplier / rate;
+            break;
+          }
+        }
+      }
+    }
   }
 }
 </script>
@@ -21,8 +65,17 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#app label {
+  display: block;
+  padding: 1rem;
+}
+#app input[type="text"]::-ms-clear {
+  display: none;
+}
+#app input[type="text"]::-webkit-clear-button{
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
